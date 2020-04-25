68203681d991b19f0e13d39f94903be44aaab164
deadprogram <ron@hybridgroup.com>
2020-04-25 09:46:29 +0200

    runtest: adjust comment formatting
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- runtest.sh
+++ runtest.sh
@@ -2,7 +2,7 @@
 stty -F /dev/ttyACM0 115200 raw -echo
 
 exec 3</dev/ttyACM0                     # redirect serial to fd3
-  cat <&3 > /tmp/testoutput.txt &          # redirect fd3 serial to file
+  cat <&3 > /tmp/testoutput.txt &       # redirect fd3 serial to file
   PID=$!                                # save pid to kill test if needed
     echo "t" > /dev/ttyACM0             # send command to start tests
     sleep 1.0s                          # wait for response
@@ -10,4 +10,4 @@ exec 3</dev/ttyACM0                     # redirect serial to fd3
   wait $PID 2>/dev/null                 # surpress "Terminated" output
 
 exec 3<&-                               # free fd3
-cat /tmp/testoutput.txt                    # display test results
+cat /tmp/testoutput.txt                 # display test results
