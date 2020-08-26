e668accea81f3a926b1ff309b8bf7f6fa92a31df
deadprogram <ron@hybridgroup.com>
2020-08-26 19:53:39 +0200

    hifive1b: remove board for now due to HW failure
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- tools/server/boards.go
+++ tools/server/boards.go
@@ -46,13 +46,13 @@ var (
 			baud:        115200,
 			resetpause:  2 * time.Second,
 		},
-		&Board{
-			target:      "hifive1b",
-			displayname: "SiFive HiFive1 Rev.B",
-			port:        "hifive1b",
-			baud:        115200,
-			resetpause:  5 * time.Second,
-		},
+		// &Board{
+		// 	target:      "hifive1b",
+		// 	displayname: "SiFive HiFive1 Rev.B",
+		// 	port:        "hifive1b",
+		// 	baud:        115200,
+		// 	resetpause:  5 * time.Second,
+		// },
 		&Board{
 			target:      "circuitplay-express",
 			displayname: "Adafruit Circuit Playground Express",
