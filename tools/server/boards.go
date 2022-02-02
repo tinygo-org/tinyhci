a1d5a85b55a692513d30960cd7ce8149bd195b2d
deadprogram <ron@hybridgroup.com>
2022-02-02 10:17:11 +0100

    boards: reduce wait time for hifive, since does not have any effect
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- tools/server/boards.go
+++ tools/server/boards.go
@@ -56,7 +56,7 @@ var (
 			displayname: "SiFive HiFive1 Rev.B",
 			port:        "hifive1b",
 			baud:        115200,
-			resetpause:  15 * time.Second,
+			resetpause:  5 * time.Second,
 			enabled:     true,
 		},
 		&Board{
