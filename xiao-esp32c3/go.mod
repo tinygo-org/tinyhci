5b34d5bf982ac41a1a6e4a3a214ebd808e7a5683
deadprogram <ron@hybridgroup.com>
2026-02-23 19:40:35 +0100

    boards: add test support for xiao-esp32c3 board
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ xiao-esp32c3/go.mod
@@ -0,0 +1,9 @@
+module xiao-esp32c3
+
+go 1.22.1
+
+toolchain go1.24.2
+
+require tinygo.org/x/drivers v0.34.0
+
+require github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
---
+++ xiao-esp32c3/main.go
@@ -0,0 +1,199 @@
+package main
+
+// Integration tests for Xiao ESP32-C3
+//
+// Wire up the pins, and run it while connected to the USB port.
+//
+// Digital read/write tests (GPIO):
+//	D0  <--> 3V3
+//	D2  <--> D3
+//
+// I2C tests:
+// 	Xiao ESP32-C3 SCL (D5) <--> MPU6050 SCL
+// 	Xiao ESP32-C3 SDA (D4) <--> MPU6050 SDA
+// 	Xiao ESP32-C3 G        <--> MPU6050 GND
+// 	Xiao ESP32-C3 3V3      <--> MPU6050 VCC
+//
+// SPI tests:
+// 	Xiao ESP32-C3 CDO - D10 <--> Xiao ESP32-C3 CDI - D9
+//
+import (
+	"machine"
+
+	"time"
+
+	"tinygo.org/x/drivers/mpu6050"
+)
+
+var (
+	// used by digital GPIO tests
+	readV    = machine.D0
+	readpin  = machine.D2
+	writepin = machine.D3
+
+	// used by i2c tests
+	accel    *mpu6050.Device
+	powerpin = machine.D3
+)
+
+func main() {
+	waitForStart()
+
+	digitalReadVoltageGPIO()
+	digitalWriteGPIO()
+	i2cConnection()
+	spiTxRx()
+
+	endTests()
+}
+
+// wait for keypress on serial port to start test suite.
+func waitForStart() {
+	time.Sleep(5 * time.Second)
+
+	println("=== TINYGO INTEGRATION TESTS ===")
+	println("Press 't' key to begin running tests...")
+
+	for {
+		if machine.Serial.Buffered() > 0 {
+			data, _ := machine.Serial.ReadByte()
+
+			if data != 't' {
+				time.Sleep(100 * time.Millisecond)
+			}
+			return
+		}
+	}
+}
+
+func endTests() {
+	println("\n### Tests complete.")
+
+	// tests done, now sleep waiting for baud reset to load new code
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
+
+// digital read of a GPIO pin physically connected to V
+func digitalReadVoltageGPIO() {
+	printtest("digitalReadVoltage (GPIO)")
+
+	readV.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
+	time.Sleep(100 * time.Millisecond)
+
+	// should be on
+	if readV.Get() {
+		printtestresult("pass")
+		return
+	}
+
+	printtestresult("fail")
+}
+
+// digital write on/off of one GPIO pin as input physically connected to a different GPIO pin as output.
+func digitalWriteGPIO() {
+	readpin.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
+	writepin.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	time.Sleep(100 * time.Millisecond)
+
+	printtest("digitalWriteOn (GPIO)")
+	writepin.High()
+	time.Sleep(100 * time.Millisecond)
+
+	// should be on
+	if readpin.Get() {
+		printtestresult("pass")
+	} else {
+		printtestresult("fail")
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	printtest("digitalWriteOff (GPIO)")
+	writepin.Low()
+	time.Sleep(100 * time.Millisecond)
+
+	// should be off
+	if readpin.Get() {
+		printtestresult("fail")
+		return
+	} else {
+		printtestresult("pass")
+	}
+}
+
+// checks to see if an attached MPU6050 accelerometer is connected.
+func i2cConnection() {
+	machine.I2C0.Configure(machine.I2CConfig{})
+	time.Sleep(100 * time.Millisecond)
+
+	a := mpu6050.New(machine.I2C0)
+	accel = &a
+
+	printtest("i2cConnection (MPU6050)")
+
+	err := accel.Configure()
+	if err != nil {
+		printtestresult(err.Error())
+		return
+	}
+	time.Sleep(400 * time.Millisecond)
+
+	if !accel.Connected() {
+		printtestresult("fail")
+		return
+	}
+
+	printtestresult("pass")
+}
+
+// checks if it is possible to send/receive by spi
+func spiTxRx() {
+	spi0 := machine.SPI2
+	spi0.Configure(machine.SPIConfig{
+		SCK:       machine.SPI_SCK_PIN,
+		SDO:       machine.SPI_SDO_PIN,
+		SDI:       machine.SPI_SDI_PIN,
+		Frequency: 4000000,
+	})
+
+	from := make([]byte, 8)
+	for i := range from {
+		from[i] = byte(i)
+	}
+	to := make([]byte, len(from))
+
+	printtest("spiTx")
+	err := spi0.Tx(from, to)
+	if err != nil {
+		printtestresult("fail")
+	} else {
+		printtestresult("pass")
+	}
+
+	printtest("spiRx")
+	for i := range from {
+		if from[i] != to[i] {
+			printtestresult("fail")
+			return
+		}
+	}
+	printtestresult("pass")
+}
+
+func printtest(testname string) {
+	print("- " + testname + " = ")
+}
+
+func printtestresult(result string) {
+	println("***" + result + "***")
+}
+
+func printfailexpected(reason string) {
+	println("        expected:", reason)
+}
+
+func printfailactual(val uint16) {
+	println("        actual:", val)
+}
